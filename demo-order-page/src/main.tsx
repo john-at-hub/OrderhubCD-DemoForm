d2cf6f2d16e785d1f79056fedc2c08365c7fb2fc
john-at-hub <john.skarbek@hubinternational.com>
2025-04-08 12:43:16 -0500

    Set up browser router

--- demo-order-page/src/main.tsx
+++ demo-order-page/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import DatePickerTestPage from './pages/DatePickerTestPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/datepicker" element={<DatePickerTestPage />} />
+      </Routes>
+    </BrowserRouter>
     <App />
   </StrictMode>,
 )
---
+++ demo-order-page/src/pages/DatePickerTestPage.tsx
@@ -0,0 +1,6 @@
+const DatePickerTestPage = () => {
+    return <h1 className="text-2xl font-bold">Date Picker Test Page</h1>;
+  };
+  
+  export default DatePickerTestPage;
+  
