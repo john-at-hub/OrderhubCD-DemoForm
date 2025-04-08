302c473fc1d73c773b46135822b04447ced2de22
john-at-hub <john.skarbek@hubinternational.com>
2025-04-08 12:37:36 -0500

    Add react router

--- demo-order-page/src/App.tsx
+++ demo-order-page/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import { Calendar } from './components/ui/calendar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,9 @@ function App() {
       <h1>Vite + React</h1>
       <h2 className="text-3xl font-bold underline">Tailwind is workng</h2>
       <Button />
+      <div>
+        <Calendar />
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
