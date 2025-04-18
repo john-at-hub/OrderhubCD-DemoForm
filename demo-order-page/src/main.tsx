fad2640150db42645e5242c30a64628c591cd0a1
john-at-hub <john.skarbek@hubinternational.com>
2025-04-18 10:52:39 -0500

    Fix state of test form entries

--- demo-order-page/src/main.tsx
+++ demo-order-page/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import DatePickerTestPage from './pages/DatePickerTestPage.tsx';
 import FormPage from './pages/FormPage.tsx'
 
+
+
 createRoot(document.getElementById('root')!).render(
+  
   <StrictMode>
     <BrowserRouter>
       <Routes>
--- demo-order-page/src/pages/FormPage.tsx
+++ demo-order-page/src/pages/FormPage.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react"
 
 function FormPage() {
-    const [count, setCount] = useState(0)
-  
+ 
+  const [testData, setTestData] = useState([
+    {
+      name: 'Jolly',
+      number: 431,
+      locations: 'San Antonio, TX',
+      id: 1
+    },
+    {
+      name: 'Amber',
+      number: 64,
+      locations: 'Midland, TX',
+      id: 2
+    },
+  ])
+
+  const displayData = testData.map(item => <p key={item.id}>{item.name}</p>)
+       
+
     return (
       <>        
-        <h2 className="text-3xl font-bold underline">This is the form testing Page</h2>    
+        <h2 className="text-3xl font-bold underline">This is the form testing Page</h2>
+        {displayData}
       </>
     )
-  }
-  
-  export default FormPage
-  
+}
+
+export default FormPage
