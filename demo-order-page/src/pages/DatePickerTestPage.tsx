d34503dda7d767628147f6f581daadc8cfe3278c
john-at-hub <john.skarbek@hubinternational.com>
2025-04-21 10:36:34 -0500

    Set up datepicker demo

--- demo-order-page/src/pages/DatePickerTestPage.tsx
+++ demo-order-page/src/pages/DatePickerTestPage.tsx
@@ -1,5 +1,13 @@
+import { DatePickerDemo } from "@/components/custom/DatePickerDemo";
+
 const DatePickerTestPage = () => {
-    return <h1 className="text-2xl font-bold">Date Picker Test Page</h1>;
+
+    return (
+      <>
+        <h1 className="text-2xl font-bold mb-8">Date Picker Test Page</h1>
+        <DatePickerDemo />
+      </>      
+    )
   };
   
   export default DatePickerTestPage;
