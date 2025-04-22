df88f9c873d9a0b4ed9156bab9cf539adfa7cc19
john-at-hub <john.skarbek@hubinternational.com>
2025-04-22 13:29:38 -0500

    Create route to a page for displaying data

--- demo-order-page/src/main.tsx
+++ demo-order-page/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.tsx'
 import DatePickerTestPage from './pages/DatePickerTestPage.tsx';
 import { GeneratedForm } from './pages/FormPage.tsx';
+import DisplayData from './pages/DisplayData.tsx';
 
 
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/datepicker" element={<DatePickerTestPage />} />
         <Route path="/form-test" element={<GeneratedForm />} />
+        <Route path="/admin" element={<DisplayData />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ demo-order-page/src/pages/DisplayData.tsx
@@ -0,0 +1,10 @@
+const DisplayData = () => {
+
+    return (
+      <>
+        <h1 className="text-2xl font-bold mb-8">display the data</h1>
+      </>      
+    )
+  };
+  
+  export default DisplayData;
