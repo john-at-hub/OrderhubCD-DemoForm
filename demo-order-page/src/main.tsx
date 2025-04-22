862a3cc14f6e599f2a6e9410839ff0110cbf8d1a
john-at-hub <john.skarbek@hubinternational.com>
2025-04-22 13:37:13 -0500

    Update main.tsx to use the new RootLayout component

--- demo-order-page/src/main.tsx
+++ demo-order-page/src/main.tsx
@@ -1,24 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css'
-import App from './App.tsx'
-import DatePickerTestPage from './pages/DatePickerTestPage.tsx';
-import { GeneratedForm } from './pages/FormPage.tsx';
-import DisplayData from './pages/DisplayData.tsx';
-
-
+// main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import RootLayout from './RootLayout';
 
 createRoot(document.getElementById('root')!).render(
-  
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/datepicker" element={<DatePickerTestPage />} />
-        <Route path="/form-test" element={<GeneratedForm />} />
-        <Route path="/admin" element={<DisplayData />} />
-      </Routes>
+      <RootLayout />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './index.css'
+// import App from './App.tsx'
+// import DatePickerTestPage from './pages/DatePickerTestPage.tsx';
+// import { GeneratedForm } from './pages/FormPage.tsx';
+// import DisplayData from './pages/DisplayData.tsx';
+
+
+
+// createRoot(document.getElementById('root')!).render(
+  
+//   <StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/datepicker" element={<DatePickerTestPage />} />
+//         <Route path="/form-test" element={<GeneratedForm onSubmit={handleFormSubmit}/>} />
+//         <Route path="/admin" element={<DisplayData submissions={submissions}/>} />
+//       </Routes>
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
